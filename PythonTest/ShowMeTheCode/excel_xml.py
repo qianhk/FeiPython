745c7a8bdb6be445a3bc909a000c6ea4999b0de1
qianhk <qhk83@qq.com>
2017-10-08 14:09:00 +0800

    can write to xml

--- PythonTest/ShowMeTheCode/excel_xml.py
+++ PythonTest/ShowMeTheCode/excel_xml.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 import xlrd
-# from lxml import etree
+from lxml import etree
 import json
 import io
 import sys
@@ -17,38 +17,51 @@ def read_xls(fromfile):
 
     book = xlrd.open_workbook(fromfile)
     sheet = book.sheet_by_name('student')
-    data = {}
+    dataList = []
     rows = sheet.nrows
     cols = sheet.ncols
-    kv = []
-    for i in range(rows):
-        for j in range(1, cols):
-            if type(sheet.cell_value(i, j)) is float:
-                kv.append(int(sheet.cell_value(i, j)))
-            else:
-                kv.append(sheet.cell_value(i, j))
-        data[str(int(sheet.cell_value(i, 0)))] = kv
-    return json.dumps(data, ensure_ascii=False)
+
+    for row in range(rows):
+        dict = {}
+        dict['stu_no'] = sheet.cell_value(row, 0)
+        dict['stu_name'] = sheet.cell_value(row, 1)
+        dict['chinese'] = int(sheet.cell_value(row, 2))
+        dict['math'] = int(sheet.cell_value(row, 3))
+        dict['english'] = int(sheet.cell_value(row, 4))
+        dataList.append(dict)
+    return (dataList, json.dumps(dataList, ensure_ascii=False))
 
 
-# def to_xml(data):
-#     # Create a document and elements
-#     root = etree.Element('root')
-#     stu = etree.SubElement(root, 'student')
-#     # Create a comment
-#     stu.append(etree.Comment(u' 学生信息表\n\t"id" : [名字, 数学, 语文, 英文]'))
-#
-#     # Create text
-#     stu.text = data
-#
-#     # Save to file
-#     tree = etree.ElementTree(root)
-#     tree.write('student.xml', encoding='utf-8', pretty_print=True, \
-#                xml_declaration=True)
+def to_json_file(json):
+    with open('../cache/student_new.json', 'wt') as f:
+        f.write(json)
+
+
+def to_xml(dataList):
+    root = etree.Element('root')
+    # Create a comment
+    root.append(etree.Comment(u' 学生信息表\n\t[学号, 姓名, 语文, 数学, 英语]'))
+    stuList = etree.SubElement(root, 'student_list')
+
+    # Create student item
+    for item in dataList:
+        stu = etree.SubElement(stuList, 'stu')
+        stu.set('stu_no', item['stu_no'])
+        stu.text = item['stu_name']
+        stu.set('chinese', str(item['chinese']))
+        stu.set('math', str(item['math']))
+        stu.set('english', str(item['english']))
+
+
+    # Save to file
+    tree = etree.ElementTree(root)
+    tree.write('../cache/student_list.xml', encoding='utf-8', pretty_print=True, \
+               xml_declaration=True)
 
 
 if __name__ == '__main__':
-    # reload(sys)
-    # sys.setdefaultencoding('utf-8')
-    data = read_xls('../cache/student.xls')
-    # to_xml(data)
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    (dataList, json) = read_xls('../cache/student.xls')
+    to_json_file(json)
+    to_xml(dataList)
