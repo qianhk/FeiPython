dead1e1a53af06afeaf54c61bdc41ccc93b3659e
qianhk <qhk83@qq.com>
2017-12-24 22:45:56 +0800

    test matrix

--- Python3Test/TensorBoardTest.py
+++ Python3Test/TensorBoardTest.py
@@ -3,24 +3,56 @@
 
 import tensorflow as tf
 
-# input1 = tf.constant([1.0, 2.0, 3.0], name = 'N_input1')
-# input2 = tf.Variable(tf.random_uniform([3]), name = 'N_input2')
+# with tf.name_scope('S_input1'):
+#     input1 = tf.constant([1.0, 2.0, 3.0], name = 'N_input1')
+#
+# with tf.name_scope("S_input2"):
+#     input2 = tf.Variable(tf.random_uniform([3]), name = 'N_input2')
+#
 # output = tf.add_n([input1, input2], name = 'Nadd')
 #
 # writer = tf.summary.FileWriter("./logs/boardTest", tf.get_default_graph())
 # writer.close()
 
 
-a = tf.constant(5, name="input_a")
-b = tf.constant(3, name="input_b")
-c = tf.multiply(a, b, name="mul_c")
-d = tf.add(a, b, name="add_d")
-e = tf.add(c, d, name="add_e")
+# a = tf.constant(5, name="input_a")
+# b = tf.constant(3, name="input_b")
+# c = tf.multiply(a, b, name="mul_c")
+# d = tf.add(a, b, name="add_d")
+# e = tf.add(c, d, name="add_e")
+#
+# sess = tf.Session()
+# print(sess.run(e))
+# output = sess.run(e)
+#
+# writer = tf.summary.FileWriter('./logs/boardTest', graph=sess.graph)
+# writer.close()
+# print(sess.run(e))
 
-sess = tf.Session()
-print(sess.run(e))
-output = sess.run(e)
 
-writer = tf.summary.FileWriter('./logs/boardTest', graph=sess.graph)
-writer.close()
-print(sess.run(e))
+# with tf.variable_scope("foo"):
+#     a = tf.get_variable("bar", [1])
+#     print(a.name)
+#
+# with tf.variable_scope("bar"):
+#     b = tf.get_variable("bar", [1])
+#     print(b.name)
+#
+# with tf.name_scope('a'):
+#     a = tf.Variable([1])
+#     print(a.name)
+#
+#     a = tf.get_variable("b", [1])
+#     print(a.name)
+#
+# with tf.name_scope('b'):
+#     try:
+#         b = tf.get_variable("b", [1])
+#         print('again: ' + b.name)
+#     except ValueError as err:  # ValueError: Variable b already exists
+#         print('found exception: ')
+#     else:
+#         print('no exception')
+
+
+
--- Python3Test/TensorflowTest.py
+++ Python3Test/TensorflowTest.py
@@ -2,22 +2,78 @@
 # coding=utf-8
 
 import tensorflow as tf
+import numpy as np
 
-with tf.device('/cpu:0'):
+# with tf.device('/cpu:0'):
+#
+#     sess = tf.Session()
+#
+#     # a_gpu = tf.Variable(0, name="a_gup")
+#     # sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+#
+#     hello = tf.constant('Hello, TensorFlow!')
+#     print(sess.run(hello))
+#
+#     a = tf.constant(10)
+#     b = tf.constant(32)
+#     print(sess.run(a + b))
+#
+#     c = tf.constant('haHa')
+#     print(sess.run(c))
+#
+#     sess.close()
 
-    sess = tf.Session()
 
-    # a_gpu = tf.Variable(0, name="a_gup")
-    # sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True))
+identity_matrix = tf.diag([1.0, 3.0, 1.0])
+A = tf.truncated_normal([2, 3])
+B = tf.fill([2, 3], 5.0)
+C = tf.random_uniform([3, 2], maxval=100)
+D = tf.convert_to_tensor(np.array([[1., 2., 3.], [-3., -7., -1.], [0., 5., -2.]]))
+sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
 
-    hello = tf.constant('Hello, TensorFlow!')
-    print(sess.run(hello))
+print('\nI=')
+print(sess.run(identity_matrix))
+print('\nA=')
+print(sess.run(A))
+print('\nB=')
+print(sess.run(B))
+print('\nC=')
+C = sess.run(C)
+print(C)
+print('\nD=')
+print(sess.run(D))
 
-    a = tf.constant(10)
-    b = tf.constant(32)
-    print(sess.run(a + b))
+print('\nA+B=')
+print(sess.run(A+B))
 
-    c = tf.constant('haHa')
-    print(sess.run(c))
+print('\nB-B=')
+print(sess.run(B-B))
 
-    sess.close()
+print('\nB*I=')
+BI = tf.matmul(B, identity_matrix)
+print(sess.run(BI))
+
+print('\ntranspose(C)=')
+print(sess.run(tf.transpose(C)))
+
+print('\ntranspose(D)=')
+print(sess.run(tf.transpose(D)))
+
+print('\ninverse(D)=')
+print(sess.run(tf.matrix_inverse(D)))
+
+print('\ndeterminant(D)={:.1f}'.format(sess.run(tf.matrix_determinant(D))))
+
+print('\ncholesky(D):')
+print(sess.run(tf.cholesky(identity_matrix)))
+
+print('\nselfAdjointEig(D):')
+print(sess.run(tf.self_adjoint_eig(D)))
+
+
+sess.close()
+
+
+
+
