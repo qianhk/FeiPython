750819849a172e14b47541773f928841a5f66d09
qianhk <qhk83@qq.com>
2017-12-30 23:55:32 +0800

    test add

--- Python3Test/Way/kai_line.py
+++ Python3Test/Way/kai_line.py
@@ -12,16 +12,16 @@ sess = tf.Session()
 # 造数据 y=Kx+3 (K=5)
 
 # 数据数量
-data_amount = 101
+data_amount = 1001
 
-x_vals = np.linspace(0, 100, data_amount)
+x_vals = np.linspace(20, 100, data_amount)
 # print(x_vals)
 
-y_vals = np.multiply(x_vals, 5)
+y_vals = np.add(x_vals, 15)
 # y_vals = np.add(y_vals, 3)
 # print(y_vals)
 
-y_offset_vals = np.random.normal(0, 5, data_amount)
+y_offset_vals = np.random.normal(0, 0.1, data_amount)
 # print(y_offset_vals)
 
 y_vals = np.add(y_vals, y_offset_vals)
@@ -34,7 +34,7 @@ y_target = tf.placeholder(shape=[1], dtype=tf.float32)
 # 构造K
 K = tf.Variable(tf.random_normal(mean=10, shape=[1]))
 
-multiply = tf.multiply(x_data, K)
+multiply = tf.add(x_data, K)
 calcY = multiply #tf.add(multiply, 3)
 
 loss = tf.square(calcY - y_target)
@@ -49,12 +49,12 @@ print('init K=' + str(sess.run(K)))
 my_opt = tf.train.GradientDescentOptimizer(0.02)
 train_step = my_opt.minimize(loss)
 
-for i in range(100):
+for i in range(1000):
     rand_index = np.random.choice(data_amount)
     x = [x_vals[rand_index]]
     y = [y_vals[rand_index]]
     sess.run(train_step, feed_dict={x_data: x, y_target: y})
-    if (i + 1) % 25 != 1110:
+    if (i + 1) % 25 == 0:
         print('Step #' + str(i + 1) + ' K = ' + str(sess.run(K)))
         print('Loss = ' + str(sess.run(loss, feed_dict={x_data: x, y_target: y})))
 
