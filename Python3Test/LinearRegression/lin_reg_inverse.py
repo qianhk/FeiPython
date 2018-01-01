30fd549101efaf3b4da53e2c2bc84bf9d8c1b65b
qianhk <qhk83@qq.com>
2018-01-02 00:44:14 +0800

    test reg inverse

---
+++ Python3Test/LinearRegression/lin_reg_inverse.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+# Linear Regression: Inverse Matrix Method
+# ----------------------------------
+#
+# This function shows how to use TensorFlow to
+# solve linear regression via the matrix inverse.
+#
+# Given Ax=b, solving for x:
+#  x = (t(A) * A)^(-1) * t(A) * b
+#  where t(A) is the transpose of A
+
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+ops.reset_default_graph()
+
+# Create graph
+sess = tf.Session()
+
+# Create the data
+x_vals = np.linspace(0, 10, 11)
+y_vals = x_vals + np.random.normal(0, 0.1, 11)
+
+# Create design matrix
+matrixXVals = np.matrix(x_vals)
+x_vals_column = np.transpose(matrixXVals)
+repeat1_11 = np.repeat(1, 11)
+ones_column = np.transpose(np.matrix(repeat1_11))
+A = np.column_stack((x_vals_column, ones_column))
+
+# Create b matrix
+b = np.transpose(np.matrix(y_vals))
+
+# Create tensors
+A_tensor = tf.constant(A)
+b_tensor = tf.constant(b)
+
+# Matrix inverse solution
+tA_A = tf.matmul(tf.transpose(A_tensor), A_tensor)
+tA_A_inv = tf.matrix_inverse(tA_A)
+product = tf.matmul(tA_A_inv, tf.transpose(A_tensor))
+solution = tf.matmul(product, b_tensor)
+
+solution_eval = sess.run(solution)
+
+# Extract coefficients
+slope = solution_eval[0][0]
+y_intercept = solution_eval[1][0]
+
+print('slope: ' + str(slope))
+print('y_intercept: ' + str(y_intercept))
+
+# Get best fit line
+best_fit = []
+for i in x_vals:
+    best_fit.append(slope * i + y_intercept)
+
+# Plot the results
+plt.plot(x_vals, y_vals, 'o', label='Data')
+plt.plot(x_vals, best_fit, 'r-', label='Best fit line', linewidth=3)
+plt.legend(loc='upper left')
+plt.show()
