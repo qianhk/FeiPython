66bf066c36e32dee35472dc7f771db3d1b73b59f
qianhk <qhk83@qq.com>
2018-01-07 17:15:34 +0800

    can remove

--- sublime-remove-no-contain-lines/remove_no_contain_lines_command.py
+++ sublime-remove-no-contain-lines/remove_no_contain_lines_command.py
@@ -1,14 +1,17 @@
 import sublime
 import sublime_plugin
-import sys
+
 
 # print(sys.version)
 # print(sys.version_info)
 
-class RemoveNoContainLinesCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        # self.view.insert(edit, 0, "Hello, World2!")
-        # allAbc = self.view.find_all('Abc')
+class RemoveNoContainLinesTextCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **args):
+        print('TextCommand args')
+        print(args)
+        keep_word = args['keep_word']
+        # print(keep_word)
+
         whole_region = sublime.Region(0, self.view.size())
         if whole_region.empty():
             return
@@ -20,7 +23,7 @@ class RemoveNoContainLinesCommand(sublime_plugin.TextCommand):
         # print(lines)
         # print(type(newlines))
         for i, line in enumerate(lines):
-            if 'Abc' in line:
+            if keep_word in line:
                 newlines.append(line)
         # print(newlines)
         new_contents = '\n'.join(newlines) + '\n'
@@ -30,3 +33,26 @@ class RemoveNoContainLinesCommand(sublime_plugin.TextCommand):
         # self.view.begin_edit() #no need for st3
         self.view.replace(edit, whole_region, new_contents)
         # self.view.end_edit(edit)
+
+
+class RemoveNoContainLinesCommand(sublime_plugin.WindowCommand):
+
+    def run(self):
+        # window = sublime.active_window()
+        # self.tmpEdit = edit
+        self.window.show_input_panel('请输入需包含的字符串', '', self.on_done, None, self.on_cancel)
+        # print('after show_input_panel')
+
+    def on_cancel(self):
+        self.edit_token = None
+        self.args = None
+        sublime.status_message('cancel operation')
+
+    def on_done(self, input):
+        # self.view.insert(edit, 0, "Hello, World2!")
+        # allAbc = self.view.find_all('Abc')
+        if len(input) == 0:
+            sublime.status_message('没有输入，取消操作')
+            return
+        view = self.window.active_view()
+        view.run_command('remove_no_contain_lines_text', {'keep_word': input})
