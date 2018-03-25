125dd703ab027cb0a89b417a4141860c1a0b8690
qianhk <qhk83@qq.com>
2018-03-25 22:32:14 +0800

    test pandas

---
+++ Python3Test/Pandas/pandas_test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import pandas as pd
+
+# from https://colab.research.google.com/notebooks/mlcc/intro_to_pandas.ipynb?hl=zh-cn
+
+print('\npandas version = %s' % (pd.__version__))
+
+series = pd.Series(['KaiKai', 'YangYang', 'ChunChun'])
+
+print('\nseries =\n%s  type=%s' % (series, type(series)))
+
+city_names = pd.Series(['San Francisco', 'San Jose', 'Sacramento', 'TaiZhou'])
+population = pd.Series([852469, 1015785, 485199])  # using NaN instead
+
+dataFrame = pd.DataFrame({'City name': city_names, 'Population': population})
+
+print('\ndataFrame=\n%s' % dataFrame)
+
+california_housing_dataframe = pd.read_csv("../cache/california_housing_train.csv", sep=",")
+describe = california_housing_dataframe.describe()
+
+print('\ncalifornia_housing_dataframe describe=\n%s' % describe)
+
+# will print all record
+# print('\ncalifornia_housing_dataframe=\n%s' % california_housing_dataframe)
+
+print('\ncalifornia_housing_dataframe head()=\n%s' % california_housing_dataframe.head(10))
+
+# california_housing_dataframe.hist('housing_median_age')
+
+print('\ncalifornia_housing_dataframe.hist(\'housing_median_age\') = \n%s' % california_housing_dataframe.hist(
+    'housing_median_age'))
