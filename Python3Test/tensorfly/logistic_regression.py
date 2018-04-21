ec689c9fd672fa4ced05cc3280955036d8d9a1fb
qianhk <qhk83@qq.com>
2018-04-21 22:31:53 +0800

    logistic regression

---
+++ Python3Test/tensorfly/logistic_regression.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+california_housing_dataframe = pd.read_csv("../cache/california_housing_train.csv", sep=",")
+
+california_housing_dataframe = california_housing_dataframe.reindex(
+    np.random.permutation(california_housing_dataframe.index))
+
+
+def preprocess_features(california_housing_dataframe):
+    selected_features = california_housing_dataframe[
+        ["latitude",
+         "longitude",
+         "housing_median_age",
+         "total_rooms",
+         "total_bedrooms",
+         "population",
+         "households",
+         "median_income"]]
+    processed_features = selected_features.copy()
+    # Create a synthetic feature.
+    processed_features["rooms_per_person"] = (
+            california_housing_dataframe["total_rooms"] /
+            california_housing_dataframe["population"])
+    return processed_features
+
+
+def preprocess_targets(california_housing_dataframe):
+    output_targets = pd.DataFrame()
+    # Create a boolean categorical feature representing whether the
+    # medianHouseValue is above a set threshold.
+    output_targets["median_house_value_is_high"] = (
+            california_housing_dataframe["median_house_value"] > 265000).astype(float)
+    return output_targets
+
+
+# Choose the first 12000 (out of 17000) examples for training.
+training_examples = preprocess_features(california_housing_dataframe.head(12000))
+training_targets = preprocess_targets(california_housing_dataframe.head(12000))
+
+# Choose the last 5000 (out of 17000) examples for validation.
+validation_examples = preprocess_features(california_housing_dataframe.tail(5000))
+validation_targets = preprocess_targets(california_housing_dataframe.tail(5000))
+
+# Double-check that we've done the right thing.
+print("Training examples summary:")
+display.display(training_examples.describe())
+print("Validation examples summary:")
+display.display(validation_examples.describe())
+
+print("Training targets summary:")
+display.display(training_targets.describe())
+print("Validation targets summary:")
+display.display(validation_targets.describe())
