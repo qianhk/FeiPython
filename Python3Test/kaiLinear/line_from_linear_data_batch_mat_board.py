cbdd5016a2cd5583bf537679d98d4e14c36c4a89
qianhk <qhk83@qq.com>
2018-05-04 18:31:21 +0800

    can see loss using tensorboard

--- Python3Test/kaiLinear/line_from_linear_data_batch_mat_board.py
+++ Python3Test/kaiLinear/line_from_linear_data_batch_mat_board.py
@@ -20,7 +20,7 @@ y_calc = tf.add(tf.matmul(x_data, w), b, name='yCalc')
 
 loss = tf.square(y_calc - y_target)
 loss = tf.reduce_mean(loss)
-optimizer = tf.train.GradientDescentOptimizer(0.00003)
+optimizer = tf.train.GradientDescentOptimizer(0.0000001)
 train = optimizer.minimize(loss)
 
 sess = tf.Session()
@@ -31,32 +31,41 @@ loss_vec = []
 data_amount = len(x_data_array)
 batch_size = 20
 
-for step in range(201):
+log_dir = '../logs/line_from_linear_data'
+if tf.gfile.Exists(log_dir):
+    dir = tf.gfile.ListDirectory(log_dir)
+    for file in dir:
+        tf.gfile.Remove(log_dir + '/' + file)
+        # tf.gfile.DeleteRecursively(log_dir + '/' + file)
+# tf.gfile.MakeDirs(log_dir)
+summary_writer = tf.summary.FileWriter(log_dir, tf.get_default_graph())
+
+x = y = None
+for step in range(8_001):
     rand_index = np.random.choice(data_amount, size=batch_size)
     x = x_data_array[rand_index]
     x = np.mat(x)
     x = x.T
     y = np.transpose([y_data_array[rand_index]])
     sess.run(train, feed_dict={x_data: x, y_target: y})
-    if step % 20 == 0:
+    if step % 200 == 0:
         loss_value = sess.run(loss, feed_dict={x_data: x, y_target: y})
         loss_vec.append(loss_value)
         print('step=%d weight=%s bias=%s loss=%s' % (step, sess.run(w), sess.run(b), loss_value))
+        tf.summary.scalar('loss', loss)
+        merged = tf.summary.merge_all()
+        summary = sess.run(merged, feed_dict={x_data: x, y_target: y})
+        summary_writer.add_summary(summary, step)
 
 [[_w]] = sess.run(w)
 _w1 = sess.run(w)
 _w2 = _w1[0, 0]
 _b = sess.run(b)
 
-output_graph_def = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def,
-                                                                output_node_names=['inputX', 'yTarget', 'w', 'b',
-                                                                                   'yCalc'])
-gfile = tf.gfile.FastGFile('../logs/line_from_linear_data_batch_mat.pb', mode='wb')
-gfile.write(output_graph_def.SerializeToString())
-gfile.close()
+
+
+summary_writer.close()
 
 sess.close()
 
 print('last W=%f B=%f' % (_w, _b))
-
-kai.show_visualization_data(x_data_array, y_data_array, _w, _b, loss_vec, title='Batch Data (Matrix)')
