6537a37c17409ed6990967eecf6f6abc597b1ad9
qianhk <qhk83@qq.com>
2018-05-22 23:08:39 +0800

    test datasets.make_blobs

--- Python3Test/kaiLogistic/logistic_from_mock_blobs.py
+++ Python3Test/kaiLogistic/logistic_from_mock_blobs.py
@@ -7,20 +7,23 @@ import tensorflow as tf
 # import sklearn.datasets as datasets
 from sklearn import datasets
 
-data, target = datasets.make_circles(n_samples=100, factor=0.5, noise=0.05)
+data, target = datasets.make_blobs(n_samples=100, n_features=2, centers=2, cluster_std=1.0)
 # print('data=%s' % data)
-print('target=%s' % target)
+# print('target=%s' % target)
 
-# plt.scatter(data[:, 0], data[:, 1])
+plt.figure(figsize=(8, 12))
+ax = plt.subplot(211)
+ax.scatter(data[:, 0], data[:, 1], c=target)
 
-data *= 10
+data *= 5
 
 class1_x = [x[0] for i, x in enumerate(data) if target[i] == 1]
 class1_y = [x[1] for i, x in enumerate(data) if target[i] == 1]
 class2_x = [x[0] for i, x in enumerate(data) if target[i] != 1]
 class2_y = [x[1] for i, x in enumerate(data) if target[i] != 1]
 
-plt.scatter(class1_x, class1_y, c='r', marker='o')
-plt.scatter(class2_x, class2_y, c='b', marker='x')
+ax = plt.subplot(212)
+ax.scatter(class1_x, class1_y, c='r', marker='o')
+ax.scatter(class2_x, class2_y, c='b', marker='x')
 
 plt.show()
