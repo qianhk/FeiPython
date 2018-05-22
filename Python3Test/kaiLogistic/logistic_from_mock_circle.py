e72749451ed86d19ce947ed3d3fe76147818f1e9
qianhk <qhk83@qq.com>
2018-05-22 22:52:27 +0800

    change var name

--- Python3Test/kaiLogistic/logistic_from_mock_circle.py
+++ Python3Test/kaiLogistic/logistic_from_mock_circle.py
@@ -7,18 +7,18 @@ import tensorflow as tf
 # import sklearn.datasets as datasets
 from sklearn import datasets
 
-x_vals, y_vals = datasets.make_circles(n_samples=100, factor=0.5, noise=0.05)
-# print('x_vals=%s' % x_vals)
-print('y_vals=%s' % y_vals)
+data, target = datasets.make_circles(n_samples=100, factor=0.5, noise=0.05)
+# print('data=%s' % data)
+print('target=%s' % target)
 
-# plt.scatter(x_vals[:, 0], x_vals[:, 1])
+# plt.scatter(data[:, 0], data[:, 1])
 
-x_vals *= 100
+data *= 100
 
-class1_x = [x[0] for i, x in enumerate(x_vals) if y_vals[i] == 1]
-class1_y = [x[1] for i, x in enumerate(x_vals) if y_vals[i] == 1]
-class2_x = [x[0] for i, x in enumerate(x_vals) if y_vals[i] != 1]
-class2_y = [x[1] for i, x in enumerate(x_vals) if y_vals[i] != 1]
+class1_x = [x[0] for i, x in enumerate(data) if target[i] == 1]
+class1_y = [x[1] for i, x in enumerate(data) if target[i] == 1]
+class2_x = [x[0] for i, x in enumerate(data) if target[i] != 1]
+class2_y = [x[1] for i, x in enumerate(data) if target[i] != 1]
 
 plt.scatter(class1_x, class1_y, c='r', marker='o')
 plt.scatter(class2_x, class2_y, c='b', marker='x')
