6e01c02c44ae86deb4f0129a512f6acd5c21f982
qianhk <qhk83@qq.com>
2018-06-26 11:51:32 +0800

    test again

--- Python3Test/kaiLinear/kai_regular.py
+++ Python3Test/kaiLinear/kai_regular.py
@@ -7,35 +7,52 @@ import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
-n = 14
-pie_size = np.arange(1, n + 1, 1)
-pie_price = np.log(pie_size) * 10 + 2
-# pie_price += np.random.normal(0, 1.5, [n])
-
 b = tf.Variable(0, dtype=tf.float32)
 w1 = tf.Variable(0, dtype=tf.float32)
 w2 = tf.Variable(0, dtype=tf.float32)
 w3 = tf.Variable(0, dtype=tf.float32)
 w4 = tf.Variable(0, dtype=tf.float32)
+w5 = tf.Variable(0, dtype=tf.float32)
 
-method = 1
+method = 3
 
-y_array = w1 * pie_size + b
+if method != 3:
+    n = 14
+    pie_size = np.arange(1, n + 1, 1)
+    pie_price = np.log(pie_size) * 10 + 2
+    # pie_price += np.random.normal(0, 1.5, [n])
+else:
+    pie_size = np.array([1, 2, 3])
+    pie_price = np.log(pie_size) * 1
 
-loss = tf.reduce_mean(tf.square(y_array - pie_price))
 
-train_step = 0
-learn_rate = 0
+def loss_for_underfitting():
+    target = b + w1 * pie_size
+    return tf.reduce_mean(tf.square(target - pie_price))
+
+
+def loss_for_ok():
+    target = b + w1 * pie_size + w2 * pie_size ** 2
+    return tf.reduce_mean(tf.square(target - pie_price))
+
+
+def loss_for_overfitting():
+    target = b + w1 * pie_size + w2 * pie_size ** 2 + w3 * pie_size ** 3 + w4 * pie_size ** 4 + w5 * pie_size ** 5
+    return tf.reduce_mean(tf.square(target - pie_price))
+
 
 if method == 1:
     learn_rate = 0.01
     train_step = 1001
+    loss = loss_for_underfitting()
 elif method == 2:
-    learn_rate = 0.001
-    train_step = 2001
+    learn_rate = 0.003
+    train_step = 10001
+    loss = loss_for_ok()
 else:
-    learn_rate = 0.001
-    train_step = 4001
+    learn_rate = 0.00001
+    train_step = 10001
+    loss = loss_for_overfitting()
 
 optimizer = tf.train.GradientDescentOptimizer(learn_rate)
 train = optimizer.minimize(loss)
@@ -50,20 +67,25 @@ for step in range(train_step):
 
 _b = sess.run(b)
 _w1 = sess.run(w1)
+_w2 = sess.run(w2)
+_w3 = sess.run(w3)
+_w4 = sess.run(w4)
+_w5 = sess.run(w5)
 
-print(f'bias={_b} w1={_w1}')
+print(f'bias={_b} w1={_w1} w2={_w2} w3={_w3} w4={_w4} w5={_w5}')
 
 sess.close()
 
 best_fit = []
-for value in pie_size:
-    best_fit.append(_w1 * value + _b)
+for size in pie_size:
+    best_fit.append(_b + _w1 * size + _w2 * size ** 2 + _w3 * size ** 3 + _w4 * size ** 4 + _w5 * size ** 5)
 
 print(f'best_fit={best_fit}')
 
-plt.figure()
+show_plt = 1
 
-plt.scatter(pie_size, pie_price, c='y', marker='o', label="pie")
-plt.plot(pie_size, best_fit, color='b')
-
-plt.show()
+if show_plt == 1:
+    plt.figure()
+    plt.scatter(pie_size, pie_price, c='y', marker='o', label="pie")
+    plt.plot(pie_size, best_fit, color='b')
+    plt.show()
