f79e6f93f5a96f5593e5559c31fd99905f5287af
qianhk <qhk83@qq.com>
2018-06-28 15:05:57 +0800

    success test underfiting and overfitting and l1 regular

--- Python3Test/Numpy/polyfit_test.py
+++ Python3Test/Numpy/polyfit_test.py
@@ -5,8 +5,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-CALIBRATE_X = [-0.2, -0.1, 0, 0.1, 0.2]
-CALIBRATE_Y = [5, -5, 5, -5.0, 5.0]
+# CALIBRATE_X = [-0.2, -0.1, 0, 0.1, 0.2]
+# CALIBRATE_Y = [5, -5, 5, -5.0, 5.0]
+CALIBRATE_X = [-2, -1, 0, 1, 2]
+CALIBRATE_Y = [2.83180094, 3.1157794, 3.69481874, 4.3611474, 4.47229052]
 z1 = np.polyfit(CALIBRATE_X, CALIBRATE_Y, 4)
 print(f'z1={z1}')  # [  3.33333333e+04   9.97099527e-12  -1.33333333e+03   3.43344866e-13 5.00000000e+00]
 p1 = np.poly1d(z1)
--- Python3Test/kaiLinear/kai_overfitting_regular3.py
+++ Python3Test/kaiLinear/kai_overfitting_regular3.py
@@ -14,12 +14,24 @@ w2 = tf.Variable(0, dtype=tf.float32)
 w3 = tf.Variable(0, dtype=tf.float32)
 w4 = tf.Variable(0, dtype=tf.float32)
 
-pie_size = np.array([-0.2, -0.1, 0, 0.1, 0.2], dtype=np.float32)
-pie_price = np.array([5, -5, 5, -5.0, 5.0], dtype=np.float32)
+pie_size = np.array([-2, -1, 0, 1, 2], dtype=np.float32)
+pie_price = np.log((pie_size + 3) * 10)
+pie_price += np.random.normal(0, 0.3, [5])
+# print(pie_price)
 
-regularization_strength = 0.01
+regularization_strength = 0.2
 regularization_result = regularization_strength \
                         * (tf.abs(w1) + tf.abs(w2) + tf.abs(w3) + tf.abs(w4))
+
+
+def loss_for_underfitting():
+    target = b + w1 * pie_size
+    return tf.reduce_mean(tf.square(target - pie_price))
+
+
+def loss_for_ok():
+    target = b + w1 * pie_size + w2 * pie_size ** 2
+    return tf.reduce_mean(tf.square(target - pie_price))
 
 
 def loss_for_overfitting():
@@ -28,29 +40,33 @@ def loss_for_overfitting():
 
 
 def loss_for_overfitting_scale():
-    # 0.2     0.04    0.008   0.0016
-    # 0.1    0.01   0.001  0.0001
-    target = b + w1 * pie_size + w2 * pie_size ** 2 * 10 + w3 * pie_size ** 3 * 100 + w4 * pie_size ** 4 * 1000
+    # 2   4   8   16
+    target = b + w1 * pie_size + w2 * pie_size ** 2 / 2 + w3 * pie_size ** 3 / 4 + w4 * pie_size ** 4 / 8
     return tf.reduce_mean(tf.abs(target - pie_price))
 
 
 def loss_for_overfitting_regular():
-    target = b + w1 * pie_size + w2 * pie_size ** 2 * 10 + w3 * pie_size ** 3 * 100 + w4 * pie_size ** 4 * 1000
+    target = b + w1 * pie_size + w2 * pie_size ** 2 / 2 + w3 * pie_size ** 3 / 4 + w4 * pie_size ** 4 / 8
     return tf.reduce_mean(tf.abs(target - pie_price)) + regularization_result
 
 
-method = 2
+method = 1
 
-if method == 1:
-    learn_rate = 0.000001
-    loss = loss_for_overfitting()
+if method == 0:
+    learn_rate = 0.01
+    loss = loss_for_underfitting()
+elif method == 1:
+    learn_rate = 0.0001
+    loss = loss_for_ok()
 elif method == 2:
+    learn_rate = 0.0001
+    loss = loss_for_overfitting()
+elif method == 10:
     learn_rate = 0.05
     loss = loss_for_overfitting_scale()
 else:
-    learn_rate = 0.05
+    learn_rate = 0.001
     loss = loss_for_overfitting_regular()
-    # loss += regularization_result
 
 optimizer = tf.train.GradientDescentOptimizer(learn_rate)
 train = optimizer.minimize(loss)
@@ -59,18 +75,23 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 step = 0
+last_loss1 = 0
+last_loss2 = 0
 while True:
     step += 1
     sess.run(train)
-    if step % 10000 == 0:
+    if step % 1_0000 == 0:
         loss_value = sess.run(loss)
         time_str = time.strftime("%H:%M:%S", time.localtime())  # %Y-%m-%d %H:%M:%S
-        print(f'step={step} time={time_str} loss={loss_value:0.5f}'
+        print(f'step={step} time={time_str} loss={loss_value:0.8f}'
               + f' bias={sess.run(b):0.2f} w1={sess.run(w1):0.2f} w2={sess.run(w2):0.2f} w3={sess.run(w3):0.2f} w4={sess.run(w4):0.2f}')
-        # 如果要小于0.01 对于method2，当learn_rate = 0.01 需要88万次训练，如果提高learn_rate，则loss到达一定值的时候不会再降低
-        if loss_value < 0.1:
+        if loss_value < 0.01:
             break
-    if step >= 30_0000:
+        if last_loss1 == last_loss2 and last_loss2 == loss_value:
+            break
+        last_loss2 = last_loss1
+        last_loss1 = loss_value
+    if step >= 10_0000:
         break
 
 _b = sess.run(b)
@@ -84,9 +105,12 @@ print(f'bias={_b} w1={_w1} w2={_w2} w3={_w3} w4={_w4}')
 sess.close()
 
 best_fit = []
-x_array = np.linspace(pie_size[0] - 0.01, pie_size[len(pie_size) - 1] + 0.01, 10000)
+x_array = np.linspace(pie_size[0] - 2, pie_size[len(pie_size) - 1] + 2, 10000)
 for x in x_array:
-    best_fit.append(_b + _w1 * x + _w2 * x ** 2 * 10 + _w3 * x ** 3 * 100 + _w4 * x ** 4 * 1000)
+    if method < 10:
+        best_fit.append(_b + _w1 * x + _w2 * x ** 2 + _w3 * x ** 3 + _w4 * x ** 4)
+    else:
+        best_fit.append(_b + _w1 * x + _w2 * x ** 2 / 2 + _w3 * x ** 3 / 4 + _w4 * x ** 4 / 8)
 
 # print(f'best_fit={best_fit}')
 
