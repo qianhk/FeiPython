29bdfbcf2ed1d4c8403c279b050b2cf1084c5772
qianhk <qhk83@qq.com>
2018-07-28 23:22:37 +0800

    can run

--- Python3Test/kaiFullNN/kai_nn_circle.py
+++ Python3Test/kaiFullNN/kai_nn_circle.py
@@ -6,38 +6,41 @@ import tensorflow as tf
 from sklearn import datasets
 
 random_state = np.random.RandomState(2)
-data, target = datasets.make_circles(n_samples=100, factor=0.5, noise=0.05, random_state=random_state)
+data, target = datasets.make_circles(n_samples=100, factor=0.5, noise=0.01, random_state=random_state)
 target = np.array(target, dtype=np.float32)
 data *= 5
 # x0 = np.ones(100, dtype=np.float32)
 # print(x0)
 # m_x = np.vstack((x0, data.T))
-m_x = data.T
+m_x = data
+
+target = np.matrix(target).T
+
 # print('data=%s' % m_x)
 # print('target=%s' % target)
 
 hidden_layer_nodes = 2
-v_w1 = tf.Variable(np.zeros((2, hidden_layer_nodes)), dtype=tf.float32)
-b1 = tf.Variable(np.zeros(hidden_layer_nodes), dtype=tf.float32)
-v_w2 = tf.Variable(np.zeros((hidden_layer_nodes, 1)), dtype=tf.float32)
-b2 = tf.Variable(np.zeros(1), dtype=tf.float32)
+v_w1 = tf.Variable(np.ones((2, hidden_layer_nodes)), dtype=tf.float32)
+b1 = tf.Variable(np.ones(hidden_layer_nodes), dtype=tf.float32)
+v_w2 = tf.Variable(np.ones((hidden_layer_nodes, 1)), dtype=tf.float32)
+b2 = tf.Variable(np.ones(1), dtype=tf.float32)
 
 h_x = tf.placeholder(shape=[None, 2], dtype=tf.float32)
 h_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
 z2 = tf.add(tf.matmul(h_x, v_w1), b1)  # (N, hidden_layer_nodes)
-# a2 = tf.nn.sigmoid(z2)
-a2 = tf.nn.relu(z2)
+a2 = tf.nn.sigmoid(z2)
+# a2 = tf.nn.relu(z2)
 
 z3 = tf.matmul(a2, v_w2)  # (N, 1)
-# a3 = tf.nn.sigmoid(z2)
-a3 = tf.nn.relu(z2)
+a3 = tf.nn.sigmoid(z2)
+# a3 = tf.nn.relu(z2)
 
 # loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=z3, labels=h_target)
 loss = tf.square(h_target - a3)
 loss = tf.reduce_mean(loss)
 
-optimizer = tf.train.GradientDescentOptimizer(0.1)
+optimizer = tf.train.GradientDescentOptimizer(0.0003)
 train = optimizer.minimize(loss)
 
 sess = tf.Session()
@@ -48,7 +51,7 @@ batch_size = 20
 
 loss_vec = []
 
-for step in range(2001):
+for step in range(1001):
     # rand_index = np.random.choice(data_amount, size=batch_size)
     # tmp1 = var_x1[rand_index]
     # tmp2 = [tmp1]
@@ -57,13 +60,13 @@ for step in range(2001):
     # y = np.transpose([target[rand_index]])
     feed = {h_x: m_x, h_target: target}
     sess.run(train, feed_dict=feed)
-    if step % 100 == 0:
+    if step % 1000 == 0:
         loss_value = sess.run(loss, feed_dict=feed)
         loss_vec.append(loss_value)
-        print(f'step={step} w1={sess.run(v_w1)} w2={sess.run(v_w2)} loss={loss_value}')
+        print(f'step={step} w1={sess.run(v_w1).ravel()} w2={sess.run(v_w2).ravel()} b2={sess.run(b2)} loss={loss_value}')
 
-_w1 = sess.run(v_w1)
-_w2 = sess.run(v_w2)
+_w1 = sess.run(v_w1).ravel()
+_w2 = sess.run(v_w2).ravel()
 print(f'last w1={_w1} w2={_w2}')
 
 # print('target=\n%s\n predict=\n%s' % (target, probabilities))
