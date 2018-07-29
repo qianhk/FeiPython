f34f7b5dcf78ed6475f9eafa468813c6e840741b
qianhk <qhk83@qq.com>
2018-07-29 15:15:01 +0800

    can show right predict use sigmoid or relu

--- Python3Test/kaiFullNN/kai_nn_circle.py
+++ Python3Test/kaiFullNN/kai_nn_circle.py
@@ -10,9 +10,6 @@ random_state = np.random.RandomState(2)
 data, target = datasets.make_circles(n_samples=100, factor=0.5, noise=0.01, random_state=random_state)
 target = np.array(target, dtype=np.float32)
 data *= 5
-# x0 = np.ones(100, dtype=np.float32)
-# print(x0)
-# m_x = np.vstack((x0, data.T))
 m_x = data
 
 m_target = np.matrix(target).T
@@ -20,28 +17,36 @@ m_target = np.matrix(target).T
 # print('data=%s' % m_x)
 # print('target=%s' % target)
 
-hidden_layer_nodes = 2
-v_w1 = tf.Variable(np.ones((2, hidden_layer_nodes)), dtype=tf.float32)
-b1 = tf.Variable(np.ones(hidden_layer_nodes), dtype=tf.float32)
-v_w2 = tf.Variable(np.ones((hidden_layer_nodes, 1)), dtype=tf.float32)
-b2 = tf.Variable(np.ones(1), dtype=tf.float32)
+hidden_layer_nodes = 5
+v_w1 = tf.Variable(tf.random_normal([2, hidden_layer_nodes], 1), dtype=tf.float32)
+b1 = tf.Variable(tf.random_normal([hidden_layer_nodes], 1), dtype=tf.float32)
+v_w2 = tf.Variable(tf.random_normal([hidden_layer_nodes, 1]), dtype=tf.float32)
+b2 = tf.Variable(tf.random_normal([1], 1), dtype=tf.float32)
 
 h_x = tf.placeholder(shape=[None, 2], dtype=tf.float32)
 h_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 
+activation = tf.nn.sigmoid
+# activation = tf.nn.relu
+
 z2 = tf.add(tf.matmul(h_x, v_w1), b1)  # (N, hidden_layer_nodes)
-a2 = tf.nn.sigmoid(z2)
-# a2 = tf.nn.relu(z2)
+a2 = activation(z2)
 
 z3 = tf.matmul(a2, v_w2)  # (N, 1)
-a3 = tf.nn.sigmoid(z3)
-# a3 = tf.nn.relu(z2)
+a3 = activation(z3)
 
-# loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=z3, labels=h_target)
-loss = tf.square(h_target - a3)
+output = a3
+
+loss = tf.square(h_target - output)
 loss = tf.reduce_mean(loss)
 
-optimizer = tf.train.GradientDescentOptimizer(0.0003)
+learning_rate = 10.0
+total_count = 10001
+if activation == tf.nn.relu:
+    learning_rate = 0.005
+    total_count = 2_0001
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate)
 train = optimizer.minimize(loss)
 
 sess = tf.Session()
@@ -52,7 +57,7 @@ batch_size = 20
 
 loss_vec = []
 
-for step in range(1001):
+for step in range(total_count):
     # rand_index = np.random.choice(data_amount, size=batch_size)
     # tmp1 = var_x1[rand_index]
     # tmp2 = [tmp1]
@@ -64,16 +69,15 @@ for step in range(1001):
     if step % 1000 == 0:
         loss_value = sess.run(loss, feed_dict=feed)
         loss_vec.append(loss_value)
-        print(f'step={step} w1={sess.run(v_w1).ravel()} b1={sess.run(b1)}'
-              f' w2={sess.run(v_w2).ravel()} b2={sess.run(b2)} loss={loss_value}')
+        print(f'step={step} b1={sess.run(b1)} b2={sess.run(b2)} loss={loss_value}')
 
 _w1 = sess.run(v_w1).ravel()
 _w2 = sess.run(v_w2).ravel()
 print(f'last w1={_w1} w2={_w2}')
 
-probabilities = sess.run(a3, feed_dict={h_x: m_x}).ravel()
+probabilities = sess.run(output, feed_dict={h_x: m_x}).ravel()
 
-print('target=\n%s\n predict=\n%s' % (target, probabilities))
+# print('target=\n%s\n predict=\n%s' % (target, probabilities))
 
 class1_x = [x[0] for i, x in enumerate(data) if target[i] == 1]
 class1_y = [x[1] for i, x in enumerate(data) if target[i] == 1]
@@ -84,7 +88,7 @@ visualization_frame, _ = kai.make_visualization_frame(class1_x, class1_y, class2
 series_x1 = visualization_frame['x1']
 series_x2 = visualization_frame['x2']
 xx = np.c_[series_x1, series_x2]
-visual_probabilities = sess.run(a3, feed_dict={h_x: xx}).ravel()
+visual_probabilities = sess.run(output, feed_dict={h_x: xx}).ravel()
 visualization_frame['probabilities'] = visual_probabilities
 
 sess.close()
