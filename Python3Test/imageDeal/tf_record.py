050c04ee934c975f1178e9611c5673ff348ede4d
qianhk <qhk83@qq.com>
2018-09-02 15:28:58 +0800

    convert mnist data to tfrecorod format

---
+++ Python3Test/imageDeal/tf_record.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import tensorflow as tf
+import os
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+mnist = input_data.read_data_sets('../cache/mnist/', dtype=tf.uint8, one_hot=True)
+images = mnist.train.images
+labels = mnist.train.labels
+pixels = images.shape[1]
+num_examples = mnist.train.num_examples
+
+filename = '../logs/imageDeal/mnist_train.tfrecords'
+# print(os.path.dirname(filename))
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+writer = tf.python_io.TFRecordWriter(filename)
+for index in range(num_examples):
+    image_raw = images[index].tostring()
+    labelIndex = np.argmax(labels[index])
+    example = tf.train.Example(features=tf.train.Features(feature={
+        'pixels': _int64_feature(pixels),
+        'label': _int64_feature(labelIndex),
+        'image_raw': _bytes_feature(image_raw)
+    }))
+
+    string = example.SerializeToString()
+    writer.write(string)
+
+writer.close()
