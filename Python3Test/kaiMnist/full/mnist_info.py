280d30af3f3384d52b347426a42c1ee5521c6f9e
qianhk <qhk83@qq.com>
2018-10-14 19:13:47 +0800

    amend string

--- Python3Test/kaiMnist/full/mnist_info.py
+++ Python3Test/kaiMnist/full/mnist_info.py
@@ -6,15 +6,15 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('../../cache/mnist/', one_hot=True)
 train = mnist.train
 print(f'Train data size: {train.num_examples}')
-print(f'Train data size: {mnist.validation.num_examples}')
-print(f'Train data size: {mnist.test.num_examples}')
+print(f'Validation data size: {mnist.validation.num_examples}')
+print(f'Test data size: {mnist.test.num_examples}')
 
 item = train.images[0]
-# print(f'Example training data: {item}')
+print(f'Example training data: {item.shape}')
 
 label = train.labels[0]
-print(f'Example training data label: {label}')
+print(f'Example training data label: {label} {label.shape}')
 
 batch_size = 8
-xs, ys = mnist.train.next_batch(batch_size)
+xs, ys = mnist.train.next_batch(batch_size=batch_size)
 print(f'X shape: {xs.shape} Y shape: {ys.shape}')
