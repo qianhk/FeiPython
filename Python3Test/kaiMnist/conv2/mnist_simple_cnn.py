0a7e9a1c964c0d26263d026420a7d8846abf6a32
qianhk <qhk83@qq.com>
2018-10-20 21:12:47 +0800

    other cnn for test

--- Python3Test/kaiMnist/conv2/mnist_simple_cnn.py
+++ Python3Test/kaiMnist/conv2/mnist_simple_cnn.py
@@ -55,3 +55,68 @@ full1_weight = tf.Variable(tf.truncated_normal([full1_input_size, fully_connecte
 full1_bias = tf.Variable(tf.truncated_normal([fully_connected_size1], stddev=0.1, dtype=tf.float32))
 full2_weight = tf.Variable(tf.truncated_normal([fully_connected_size1, target_size], stddev=0.1, dtype=tf.float32))
 full2_bias = tf.Variable(tf.truncated_normal([target_size], stddev=0.1, dtype=tf.float32))
+
+
+def my_conv_net(input_data):
+    conv1 = tf.nn.conv2d(input_data, conv1_weight, strides=[1, 1, 1, 1], padding='SAME')
+    relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1_bias))
+    max_pool1 = tf.nn.max_pool(relu1, ksize=[1, max_pool_size1, max_pool_size1, 1], strides=[1, max_pool_size1, max_pool_size1, 1],
+                               padding='SAME')
+
+    conv2 = tf.nn.conv2d(max_pool1, conv2_weight, strides=[1, 1, 1, 1], padding='SAME')
+    relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2_bias))
+    max_pool2 = tf.nn.max_pool(relu2, ksize=[1, max_pool_size2, max_pool_size2, 1], strides=[1, max_pool_size2, max_pool_size2, 1],
+                               padding='SAME')
+
+    final_conv_shape = max_pool2.get_shape().as_list()
+    final_shape = final_conv_shape[1] * final_conv_shape[2] * final_conv_shape[3]
+    flat_output = tf.reshape(max_pool2, [final_conv_shape[0], final_shape])
+    fully_connected1 = tf.nn.relu(tf.add(tf.matmul(flat_output, full1_weight), full1_bias))
+    final_model_output = tf.add(tf.matmul(fully_connected1, full2_weight), full2_bias)
+    return final_model_output
+
+
+model_output = my_conv_net(x_input)
+test_model_out = my_conv_net(eval_input)
+loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=model_output, labels=y_target))
+
+prediction = tf.nn.softmax(model_output)
+test_prediction = tf.nn.softmax(test_model_out)
+
+
+def get_accuracy(logits, targets):
+    batch_predictions = np.argmax(logits, axis=1)
+    num_correct = np.sum(np.equal(batch_predictions, targets))
+    return 100 * num_correct / batch_predictions.shape[0]
+
+
+my_optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9)
+train_step = my_optimizer.minimize(loss)
+sess.run(tf.global_variables_initializer())
+
+train_loss = []
+train_acc = []
+test_acc = []
+
+for i in range(generations):
+    rand_index = np.random.choice(len(train_xdata), size=batch_size)
+    rand_x = train_xdata[rand_index]
+    rand_x = np.expand_dims(rand_x, 3)
+    rand_y = train_labels[rand_index]
+    train_dict = {x_input: rand_x, y_target: rand_y}
+    sess.run(train_step, feed_dict=train_dict)
+    temp_train_loss, temp_train_preds = sess.run([loss, prediction], feed_dict=train_dict)
+    temp_train_acc = get_accuracy(temp_train_preds, rand_y)
+    if (i + 1) % eval_every == 0:
+        eval_index = np.random.choice(len(test_xdata), size=evaluation_size)
+        eval_x = test_xdata[eval_index]
+        eval_x = np.expand_dims(eval_x, 3)
+        eval_y = test_labels[eval_index]
+        test_dict = {eval_input: eval_x, evaL_target: eval_y}
+        test_preds = sess.run(test_prediction, feed_dict=test_dict)
+        temp_test_acc = get_accuracy(test_preds, eval_y)
+        train_loss.append(temp_train_loss)
+        train_acc.append(temp_train_acc)
+        acc_and_loss = [(i + 1), temp_train_loss, temp_train_acc, temp_test_acc]
+        acc_and_loss = [np.round(x, 2) for x in acc_and_loss]
+        print('Generation # {}. Train Loss: {:.2f}. Train Acc (Test Acc): {:.2f} ({:.2f})'.format(*acc_and_loss))
