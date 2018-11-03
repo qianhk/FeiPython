d5fe6395193b5733ad217ac3e0d14447c3815abd
qianhk <qhk83@qq.com>
2018-11-03 23:01:58 +0800

    read cifar_files

--- Python3Test/kaiCnn/cifar/cifar_train_whole.py
+++ Python3Test/kaiCnn/cifar/cifar_train_whole.py
@@ -27,3 +27,17 @@ record_length = 1 + image_vec_length
 def read_cifar_files(filename_queue, distort_images=True):
     reader = tf.FixedLengthRecordReader(record_bytes=record_length)
     key, record_string = reader.read(filename_queue)
+    record_bytes = tf.decode_raw(record_string, tf.uint8)
+    image_label = tf.cast(tf.slice(record_bytes, [0], [1]), tf.int32)
+    image_extracted = tf.reshape(tf.slice(record_bytes, [1], [image_vec_length]), [num_channel, image_height, image_width])
+    image_uint8image = tf.transpose(image_extracted, [1, 2, 0])
+    reshaped_image = tf.cast(image_uint8image, tf.float32)
+    final_image = tf.image.resize_image_with_crop_or_pad(reshaped_image, crop_width, crop_height)
+    if distort_images:
+        final_image = tf.image.random_flip_left_right(final_image)
+        final_image = tf.image.random_brightness(final_image, max_delta=63)
+        final_image = tf.image.random_contrast(final_image, lower=0.2, upper=1.8)
+    final_image = tf.image.per_image_standardization(final_image)
+    return final_image, image_label
+
+
