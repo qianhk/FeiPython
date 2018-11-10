d15b4e3ae47eeddcd97a10082e2f4d5d333bf692
qianhk <qhk83@qq.com>
2018-11-10 20:44:01 +0800

    test save image to objects dir

--- Python3Test/kaiCnn/retraining/down_cifar_python.py
+++ Python3Test/kaiCnn/retraining/down_cifar_python.py
@@ -45,7 +45,7 @@ if os.path.exists(extract_folder):
 else:
     extract_files_set = set()
 
-need_files_set = set([f'data_batch_{i}' for i in range(1, 5)])
+need_files_set = set([f'data_batch_{i}' for i in range(1, 6)])
 need_files_set.add('batches.meta')
 need_files_set.add('test_batch')
 
@@ -65,22 +65,44 @@ if len(intersection_set) < len(need_files_set):
 
 objects = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-train_folder = os.path.join(extract_folder, 'train_dir')
-if not os.path.isdir(train_folder):
-    for i in range(10):
-        folder = os.path.join(train_folder, objects[i])
-        os.makedirs(folder)
-
-test_folder = os.path.join(extract_folder, 'validation_dir')
-if not os.path.isdir(test_folder):
-    for i in range(10):
-        folder = os.path.join(test_folder, objects[i])
-        os.makedirs(folder)
-
 
 def load_batch_from_file(file):
     file_conn = open(file, 'rb')
-    image_dictionary = cPickle.load(file_conn, encoding='latin1')
+    _image_dict = cPickle.load(file_conn, encoding='latin1')
     file_conn.close()
-    return image_dictionary
+    return _image_dict
 
+
+def save_images_from_dict(_image_dict, _folder):
+    _labels = _image_dict['labels']
+    _filenames = _image_dict['filenames']
+    _data = image_dict['data']
+    for ix, label in enumerate(_labels):
+        _folder_path = os.path.join(_folder, objects[label])
+        _filename = _filenames[ix]
+        _image_array = _data[ix]
+        _image_array.resize([3, 32, 32])
+        # _image_array2 = _image_array.transpose()
+        _image_array3 = _image_array.transpose([1, 2, 0])
+        output_location = os.path.join(_folder_path, _filename)
+        scipy.misc.imsave(output_location, _image_array3)
+
+
+train_folder = os.path.join(extract_folder, 'train_dir')
+train_names = ['data_batch_' + str(x) for x in range(1, 6)]
+if not os.path.isdir(train_folder):
+    for i in range(len(objects)):
+        folder = os.path.join(train_folder, objects[i])
+        os.makedirs(folder)
+    for file in train_names:
+        print(f'Saving images from file: {file}')
+        file_location = os.path.join(extract_folder, file)
+        image_dict = load_batch_from_file(file_location)
+        save_images_from_dict(image_dict, train_folder)
+
+test_folder = os.path.join(extract_folder, 'validation_dir')
+test_names = ['test_Batch']
+if not os.path.isdir(test_folder):
+    for i in range(len(objects)):
+        folder = os.path.join(test_folder, objects[i])
+        os.makedirs(folder)
