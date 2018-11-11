35b7cae97413fd2198ef38dd701e5ce9926083f4
qianhk <qhk83@qq.com>
2018-11-11 21:17:03 +0800

    try to test inception retrain but failed

---
+++ Python3Test/kaiCnn/retraining/build_image_data.sh
@@ -0,0 +1,1 @@
+python3 /AI/models/research/inception/inception/data/build_image_data.py --train_directory='../../cache/cifar-10-batches-py/train_dir' --validation_directory='../../cache/cifar-10-batches-py/validation_dir' --output_directory='../../cache/cifar-10-batches-py/temp/' --labels_file='../../cache/cifar-10-batches-py/cifar10_labels.txt'
