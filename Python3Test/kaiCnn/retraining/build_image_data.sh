14d0640e976744369a5c7433feb062a4f82bc8c1
qianhk <qhk83@qq.com>
2018-11-14 21:04:36 +0800

    add sh train

--- Python3Test/kaiCnn/retraining/build_image_data.sh
+++ Python3Test/kaiCnn/retraining/build_image_data.sh
@@ -1,1 +1,3 @@
+#!/usr/bin/env bash
+
 python3 /AI/models/research/inception/inception/data/build_image_data.py --train_directory='../../cache/cifar-10-batches-py/train_dir' --validation_directory='../../cache/cifar-10-batches-py/validation_dir' --output_directory='../../cache/cifar-10-batches-py/temp/' --labels_file='../../cache/cifar-10-batches-py/cifar10_labels.txt'
--- Python3Test/kaiCnn/retraining/inception_flowers_train.sh
+++ Python3Test/kaiCnn/retraining/inception_flowers_train.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+# flowers train 迁移
+
 # cd /AI/models/research/inception/inception/
 python3 ./flowers_train.py --train_dir='/OnGit/FeiPython/Python3Test/cache/cifar-10-batches-py/temp/training_results' --data-dir='/OnGit/FeiPython/Python3Test/cache/cifar-10-batches-py/temp/data_dir' --pretrained_model_checkpoint_path='/AI/inception_v3.ckpt' --fine_tune=True --initial_learning_rate=0.001 --input_queue_memory_factor=1
