3336106e158eda4e1fd248c970aec93847b7380d
qianhk <qhk83@qq.com>
2019-05-07 15:03:18 +0800

    can found public .h file

--- Python3Test/OC-h/extract_publish_h.py
+++ Python3Test/OC-h/extract_publish_h.py
@@ -39,8 +39,20 @@ def parse_pubic_h_file(pbxproj):
 
             line = reader.readline()
 
-        print(f'header strings:\n{header_strings}')
-        print(f'build strings:\n{build_strings}')
+            # print(f'header strings:\n{header_strings}')
+            # print(f'build strings:\n{build_strings}')
+
+        pattern = re.compile('(\w+) /\* (.+) in Headers')
+        result = pattern.findall(header_strings)
+
+        public_list = []
+        for group in result:
+            if re.search(group[0] + '.+Public,', build_strings):
+                # print(f'code={group[0]} name={group[1]}')
+                public_list.append(group[1])
+
+        # print(f'public .h, size={len(public_list)} list={public_list}')
+        return public_list
 
 
 if __name__ == '__main__':
@@ -56,12 +68,23 @@ if __name__ == '__main__':
     )
 
     parser.add_argument(
+        '-f', '--framework',
+        type=str,
+        default='',
+        # metavar='pbxproj',
+        # dest='simple_value',
+        required=True,
+        help='framework name, example: UIKit'
+    )
+
+    parser.add_argument(
         '-d', '--replacedir',
         type=str,
         default=[],
         nargs='+',
         # action='append',
         metavar='dir',
+        required=True,
         help='.h .m .mm be replaced in these dir'
     )
 
@@ -72,12 +95,17 @@ if __name__ == '__main__':
 
     args, unparsed = parser.parse_known_args()
     # print(f'sys.argv={sys.argv}\nargs={args}\nunparsed={unparsed}\nargs.pbxproj={args.pbxproj}\nargs.replacedir={args.replacedir}')
+    print(f'pbxproj={args.pbxproj} framwork={args.framework} replacedir={args.replacedir}')
 
     exist = os.path.isfile(args.pbxproj)
     if exist:
         # print(f'File "{args.pbxproj}" exists.')
         if os.access(args.pbxproj, os.R_OK):
-            parse_pubic_h_file(args.pbxproj)
+            public_list = parse_pubic_h_file(args.pbxproj)
+            if len(public_list) > 0:
+                print(f'found {len(public_list)} public .h file')
+            else:
+                print(f'public .h config not found.')
         else:
             print(f'File "{args.pbxproj}" is not accessible to read')
     else:
